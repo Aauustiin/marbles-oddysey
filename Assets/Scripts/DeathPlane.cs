30521f489c2bd919d3a0f35870a4d3d02578e570
Aauustiin <longaustin2001@gmail.com>
2022-02-13 21:54:04 +0000

    Basic death plane working

---
+++ Assets/Scripts/DeathPlane.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeathPlane : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
+        {
+            collisionInfo.transform.SetPositionAndRotation(Vector3.zero, Quaternion.Euler(Vector3.zero));
+        }
+    }
+}
