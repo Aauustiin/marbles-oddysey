b126d083e3d47eeecfd5d4ed78ed1eae8db78eff
Aauustiin <longaustin2001@gmail.com>
2022-02-17 14:55:53 +0000

    Player bonks on NPC

--- Assets/Scripts/InteractableController.cs
+++ Assets/Scripts/InteractableController.cs
@@ -6,12 +6,15 @@ public class InteractableController : MonoBehaviour
 {
     [SerializeField] UIManager uiManager;
     [SerializeField] NPCData npcData;
+    [SerializeField] float bonkMagnitude;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             uiManager.DisplayDialog(npcData.dialog);
+            Vector3 bonkDirection = collisionInfo.transform.position - transform.position;
+            collisionInfo.rigidbody.AddForce((bonkDirection.normalized + transform.up * 0.5f) * bonkMagnitude, ForceMode.Impulse);
         }
     }
 }
