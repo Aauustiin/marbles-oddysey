29b80fcfce5a29fa3e6260bc6535bacf9d55f426
Aauustiin <longaustin2001@gmail.com>
2022-02-17 15:00:58 +0000

    Player bonks on hazards

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -5,12 +5,15 @@ using UnityEngine;
 public class DamagePlayer : MonoBehaviour
 {
     [SerializeField] PlayerController player;
+    [SerializeField] float bonkMagnitude;
 
     void OnCollisionEnter(Collision collisionInfo)
     {
         if (collisionInfo.collider.gameObject.layer == LayerMask.NameToLayer("Player"))
         {
             player.TakeDamage();
+            Vector3 bonkDirection = collisionInfo.transform.position - transform.position;
+            collisionInfo.rigidbody.AddForce((bonkDirection.normalized + transform.up * 0.5f) * bonkMagnitude, ForceMode.Impulse);
         }
     }
 }
